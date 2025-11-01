586befaf2ce3d29f8bb1f70ea2e6d3f4d262d473
valencuadra <cuadradosoriano@gmail.com>
2025-11-01 03:29:54 -0300

    caca :D

--- src/main/java/net/testo/testmod/TestMod.java
+++ src/main/java/net/testo/testmod/TestMod.java
@@ -7,7 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestMod implements ModInitializer {
-	public static final String MOD_ID = "prueba";
+	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
